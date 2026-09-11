2c45ca592a8b82f973c6ccb199a54729a5b9043e
Manu <manuelr0ficial@gmail.com>
2026-09-11 16:52:14 +0200

    add meme generator help screen

--- source/main.c
+++ source/main.c
@@ -5,34 +5,20 @@
 #include <stdlib.h>
 
 #define WAIT_TIMEOUT 300000000ULL
+#define WIDTH 400
+#define HEIGHT 240
+#define SCREEN_SIZE WIDTH * HEIGHT * 2
+#define BUF_SIZE SCREEN_SIZE * 2
 
 //Variables globales
 u8 *audios[5];
 long audioSizes[5];
 int idx = 4;
 char meme_messages[4][128] = {"\x1b[16;14HTIENES 14?? ACTIVA CAM!!", "\x1b[16;17HSALE BALATRITO??", "\x1b[16;14HHAPPY BIRTHDAY DANIEL!!", "\x1b[16;11HWHAT IS THIS DIDDYBLUD DOING??"};
+char *memeNames[5] = {"Tienes 14 activa cam", "Sale balatrito?", "Aura monster", "Diddyblud"};
+int selectedMemeIdx = 0;
 static ndspWaveBuf waveBuf;
 
-
-void writePictureToFramebufferRGB565(void *fb, void *img, u16 x, u16 y, u16 width, u16 height) {
-	u8 *fb_8 = (u8*) fb;
-	u16 *img_16 = (u16*) img;
-	int i, j, draw_x, draw_y;
-	for(j = 0; j < height; j++) {
-		for(i = 0; i < width; i++) {
-			draw_y = y + height - j;
-			draw_x = x + i;
-			u32 v = (draw_y + draw_x * height) * 3;
-			u16 data = img_16[j * width + i];
-			uint8_t b = ((data >> 11) & 0x1F) << 3;
-			uint8_t g = ((data >> 5) & 0x3F) << 2;
-			uint8_t r = (data & 0x1F) << 3;
-			fb_8[v] = r;
-			fb_8[v+1] = g;
-			fb_8[v+2] = b;
-		}
-	}
-}
    
 inline void clearScreen(){
 
@@ -61,42 +47,70 @@ void printMenuScreen(void){
     puts("\x1B[1;33mWelcome to Meme Scroller v1.1.3 by HFMaker!\x1b[0m");
     puts("");
     puts("");
-    puts("\x1B[1;37m-Press\x1B[0m \x1B[1;36mright or left D-Pad/C-Pad\x1B[0m \x1B[1;37mto scroll\nthrough memes\x1B[0m");
+    puts("\x1B[1;37m-Use\x1B[0m \x1B[1;36mright or left D-Pad/C-Pad\x1B[0m \x1B[1;37mto scroll\nthrough memes\x1B[0m");
     puts("");
     puts("\x1B[1;37m-Press\x1B[0m \x1B[1;32mB\x1B[0m \x1B[1;37mon a meme to return to this menu\x1B[0m");
     puts("");
     puts("\x1B[1;37m-Press\x1B[0m \x1B[1;35mX\x1B[0m \x1B[1;37mon a meme to use the keyboard\x1B[0m");
     puts("");
+    puts("\x1B[1;37m-Press\x1B[0m \x1B[1;33mL\x1B[0m \x1B[1;37mto enter Meme Generator mode\x1B[0m");
+    puts("");
     puts("\x1B[1;37m-Press\x1B[0m \x1B[1;31mSTART\x1B[0m \x1B[1;37mto exit\x1B[0m");
+
+}
+
+void printMemeGenHelpScreen(void){
+
+    puts("\x1B[1;33mMeme Generator Help:\x1b[0m");
+    puts("");
+    puts("");
+    puts("\x1B[1;37m-Press\x1B[0m \x1B[1;32mB\x1B[0m \x1B[1;37mto quit\x1B[0m");
+    puts("");
+    puts("\x1B[1;37m-Press\x1B[0m \x1B[1;35mX\x1B[0m \x1B[1;37mto use the keyboard\x1B[0m");
+    puts("");
+    puts("\x1B[1;37m-Use the\x1B[0m \x1B[1;36mTouch Screen\x1B[0m \x1B[1;37mto drag the messages\x1B[0m");
+    puts("");
+    puts("\x1B[1;37m-Press\x1B[0m \x1B[1;33mA\x1B[0m \x1B[1;37mto hide this help\x1B[0m");
+    puts("");
+    puts("\x1B[1;37m-Press\x1B[0m \x1B[1;31mY\x1B[0m \x1B[1;37mto save the image as BMP\x1B[0m");
+
+}
+
+void printMemeGenScreenMessage(void){
+
+    puts("\x1B[1;33mMeme Generator mode activated!\x1b[0m");
+    puts("");
+    puts(""); 
+    puts("\x1B[1;37m-Press\x1B[0m \x1B[1;32mB\x1B[0m to go back to the main menu");
+    puts("");
+    puts("\x1B[1;37m-Move with\x1B[0m \x1B[1;36mup and down D-Pad\x1B[0m");
+    puts("");
+    puts("\x1B[1;37m-Select with A the meme you want as a template:\x1B[0m");
+    puts("");
 
 }
 
 void printMemeGenScreen(void){
 
-    puts("\x1B[1;37mMeme Generator mode activated!\x1b[0m");
-    puts("");
-    puts(""); 
-    puts("\x1B[1;37m-Press\x1B[0m \x1B[1;32mB\x1B[0m to go back to the main menu");
-    puts("");
-    puts("\x1B[1;37m-Select the meme you want to use as a template:\x1B[0m");
-    puts("");
-
-
+    printMemeGenScreenMessage();
+    for (int i = 0; i < 4; i++) i == selectedMemeIdx ? printf("> \x1B[1;31m%s\x1B[0m\n", memeNames[i]) : puts(memeNames[i]);
 }
+
 
 void loadImage(int idx){
 
     u8 *img = malloc(320 * 240 * 3);
-    if (!img){printf("Error while trying to load the image"); free(img);} 
+    if (!img) return; 
     char path[64];
     snprintf(path, sizeof(path), "romfs:/img/meme%d.bin", idx);
     FILE *f = fopen(path, "rb");
-    if (!f){printf("Image not found or path is wrong");}
+    if (!f){free(img);}
     fread(img, 1, 320 * 240 * 3, f);
     fclose(f);
 
     u8 *fb = gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL);
     memcpy(fb, img, 320 * 240 * 3);
+    free(img);
 
 }
 
@@ -123,13 +137,18 @@ void playAudio(int idx){
 int main(void) {
     //Se inicializa los gráficos y el romfs
     gfxInitDefault();
+    acInit();
     romfsInit();
     gfxSetDoubleBuffering(GFX_BOTTOM, false);
-    gfxSetDoubleBuffering(GFX_TOP, false);
+    gfxSetDoubleBuffering(GFX_TOP, true);
        
     // Parte del mensaje
     consoleInit(GFX_TOP, NULL);
     
+
+    u8 *buf = malloc(BUF_SIZE);
+    if (!buf) goto cleanup;
+
    
     int new_idx = 4; 
     
@@ -149,16 +168,26 @@ int main(void) {
     }
 
     
-    consoleClear();//Limpiamos la consola y escribimos el mensaje de bienvenida junto con los controles
-
-    printMenuScreen();	
-
+   
     ndspInit();
     ndspSetOutputMode(NDSP_OUTPUT_MONO);
     ndspChnReset(0);
     ndspChnSetInterp(0, NDSP_INTERP_LINEAR);
     ndspChnSetRate(0, 44100.0f);
     ndspChnSetFormat(0, NDSP_FORMAT_MONO_PCM16);
+
+    camInit();
+    CAMU_SetSize(SELECT_OUT1_OUT2, SIZE_CTR_TOP_LCD, CONTEXT_A);
+    CAMU_SetOutputFormat(SELECT_OUT1_OUT2, OUTPUT_RGB_565, CONTEXT_A);
+    CAMU_SetNoiseFilter(SELECT_OUT1_OUT2, true);   
+    CAMU_SetAutoExposure(SELECT_OUT1_OUT2, true);
+    CAMU_SetAutoWhiteBalance(SELECT_OUT1, true);
+    CAMU_SetTrimming(PORT_CAM1, false);
+    CAMU_SetTrimming(PORT_CAM2, false);
+
+    consoleClear();//Limpiamos la consola y escribimos el mensaje de bienvenida junto con los controles
+
+    printMenuScreen();	
 
     float mix[12];
     memset(mix, 0, sizeof(mix));
@@ -243,27 +272,19 @@ int main(void) {
             printCenteredText(meme_messages[idx], 16);
 	        printf("\x1b[30;16H\x1B[1;37mPress\x1B[0m \x1B[1;31mStart\x1B[0m \x1B[1;37mto exit.\x1B[0m");
 
-           
-           
-
-       
 
         }
 
 
-        if (hidKeysDown() & (KEY_L | KEY_R )){
+        if (hidKeysDown() & KEY_L){
 
         idx = 4;
-        int selectedMemeIdx = 0;
         bool memeGenMode = true, memeGenEditor = false;
-        char *memeNames[5] = {"Tienes 14 activa cam", "Sale balatrito?", "Aura monster", "Diddyblud", "Take a photo"};
-       
+               
         consoleClear();
         clearScreen();
         playAudio(idx);
-
         printMemeGenScreen();
-        for (int i = 0; i < 5; i++) i == selectedMemeIdx ? printf("> %s\n", memeNames[i]) : puts(memeNames[i]);
 
 
         while (memeGenMode){
@@ -278,85 +299,42 @@ int main(void) {
                     
                     consoleClear();
                     printMemeGenScreen();
-                    for (int i = 0; i < 5; i++) i == selectedMemeIdx ? printf("> %s\n", memeNames[i]) : puts(memeNames[i]);
+
 
                 }
 
                 if (hidKeysDown() & KEY_DDOWN){
 
-                    if (selectedMemeIdx != 4) ++selectedMemeIdx;
+                    if (selectedMemeIdx != 3) ++selectedMemeIdx;
                     
                     consoleClear();
                     printMemeGenScreen();
-                    for (int i = 0; i < 5; i++) i == selectedMemeIdx ? printf("> %s\n", memeNames[i]) : puts(memeNames[i]);
+                    
 
                 }
 
                 if (hidKeysDown() & KEY_A){
                         
+                        
                         memeGenMode = false;
                         memeGenEditor = true;
+                        bool showGuide = true;
                         consoleClear();
-                        
-                        if (selectedMemeIdx == 4){puts("");}
-
-                      
-                        /*u32 bufsize;
-                        Handle event;
-
-                        gfxFlushBuffers();
-                        gspWaitForVBlank();
-                        gfxScreenSwapBuffers(GFX_TOP, false);   
-
-                        camInit();
-                        CAMU_SetSize(SELECT_OUT1_OUT2, SIZE_CTR_TOP_LCD, CONTEXT_A);
-                        CAMU_SetOutputFormat(SELECT_OUT1_OUT2, OUTPUT_RGB_565, CONTEXT_A);
-                        CAMU_SetNoiseFilter(SELECT_OUT1_OUT2, true);   
-                        CAMU_SetAutoExposure(SELECT_OUT1, true);
-                        CAMU_SetAutoWhiteBalance(SELECT_OUT1, true);
-                        CAMU_SetTrimming(PORT_CAM1, false);
-
-                        CAMU_SetTransferBytes(PORT_BOTH, bufsize, 400, 240);
-                        
-                        u8 *buf = malloc(bufsize);
-                        if (!buf) break;
-
-                        
-                        CAMU_Activate(SELECT_OUT1_OUT2);
-                        CAMU_ClearBuffer(PORT_BOTH);
-                        CAMU_SynchronizeVsyncTiming(SELECT_OUT1, SELECT_OUT2);
-                        CAMU_StartCapture(PORT_BOTH); 
-
-                        CAMU_SetReceiving(&event, buf, PORT_CAM1, 400 * 240 * 2, (s16)bufsize);
-                        svcWaitSynchronization(event, WAIT_TIMEOUT);
-                        svcCloseHandle(event);
-
-                        CAMU_StopCapture(PORT_BOTH);
-                        CAMU_Activate(SELECT_NONE);
-                
-                    
-                        u8 *fb = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
-                        writePictureToFramebufferRGB565(fb, buf, 0, 0, 400, 240);
-                        free(buf);
-
-                        }*/
-
-
                         loadImage(selectedMemeIdx);
+                        printMemeGenHelpScreen();
 
                         while(memeGenEditor){
 
                             hidScanInput();
 
-                            if (hidKeysDown() & KEY_B){
+                            if (hidKeysHeld() & KEY_B){
 
                                 consoleClear();
                                 clearScreen();
                                 clearTopScreen();
                         
-
                                 printMemeGenScreen();
-                                for (int i = 0; i < 5; i++) i == selectedMemeIdx ? printf("> %s\n", memeNames[i]) : puts(memeNames[i]);
+
                                 memeGenMode = true; 
                                 memeGenEditor = false;
 
@@ -364,10 +342,22 @@ int main(void) {
                             }
 
 
+                            if (hidKeysDown() & KEY_A){
+
+                                if (showGuide) {consoleClear(); showGuide = false;}
+                                else if (!showGuide) {printMemeGenHelpScreen(); showGuide = true;}
+                                
+
+
+                            }
+
+                            
+
+
                             gfxFlushBuffers();
-                            gfxScreenSwapBuffers(GFX_TOP, false);
-                            gfxScreenSwapBuffers(GFX_BOTTOM, false);
                             gspWaitForVBlank();
+                            gfxSwapBuffers();
+                            
 
 
 
@@ -383,10 +373,14 @@ int main(void) {
     }
 
     //Se limpia todo lo necesario y se sale del programa
-
+cleanup:
+    clearScreen();
+    consoleClear();
     ndspChnWaveBufClear(0);
     ndspExit();
+    camExit();
     for (int i = 0; i < 5; i++) if (audios[i]) linearFree(audios[i]);
+    acExit();
     romfsExit();
     gfxExit();
     return 0;
